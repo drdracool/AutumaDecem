157ffc9164fe8fe191aa5c3c9d90a4df4432fbb3
drdracool <jadexing@foxmail.com>
2024-12-03 22:02:12 +0900

    Build backend example api and start mongoDB

--- backend/start.py
+++ backend/start.py
@@ -1,28 +1,81 @@
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
+import json
 
 app = Flask(__name__)
-client = MongoClient("mongodb://localhost:27017/")
-db = client["mydatabase"]
-users_collection = db["users"]
+employees = [
+    {"id": 1, "name": "Ashley"},
+    {"id": 2, "name": "Kate"},
+    {"id": 3, "name": "Joe"},
+]
+nextEmployeeId = 4
+# client = MongoClient("mongodb://localhost:27017/")
+# db = client["mydatabase"]
+# users_collection = db["users"]
 
 
-@app.route("/api/users", methods=["GET"])
-def get_users():
-    users = users_collection.find()
-    user_list = []
-    for user in users:
-        user_list.append({"id": user["id"], "name": user["name"]})
-    return jsonify(user_list)
+@app.route("/employees", methods=["GET"])
+def get_employees():
+    return jsonify(employees)
 
 
-@app.route("/api/users", methods=["POST"])
-def create_user():
-    data = request.get_json()
-    new_user = {"id": data["id"], "name": data["name"]}
-    users_collection.insert_one(new_user)
-    return jsonify(new_user), 201
+@app.route("/employees/<int:id>", methods=["GET"])
+def get_employee_by_id(id: int):
+    employee = get_employee(id)
+    if employee is None:
+        return jsonify({"error": "Employee does not exist"}), 404
+    return jsonify(employee)
+
+
+def get_employee(id):
+    return next((e for e in employees if e["id"] == id), None)
+
+
+def employee_is_valid(employee):
+    for key in employee.keys():
+        if key != "name":
+            return False
+    return True
+
+
+@app.route("/employees", methods=["POST"])
+def create_employee():
+    global nextEmployeeId
+    employee = json.loads(request.data)
+    if not employee_is_valid(employee):
+        return jsonify({"error": "Invalid employee properties."}), 400
+
+    employee["id"] = nextEmployeeId
+    nextEmployeeId += 1
+    employees.append(employee)
+
+    return "", 201, {"location": f'/employees/{employee["id"]}'}
+
+
+@app.route("/employees/<int:id>", methods=["PUT"])
+def update_employee(id: int):
+    employee = get_employee(id)
+    if employee is None:
+        return jsonify({"error": "Employee does not exist."}), 404
+
+    updated_employee = json.loads(request.data)
+    if not employee_is_valid(updated_employee):
+        return jsonify({"error": "Invalid employee properties."}), 400
+
+    employee.update(updated_employee)
+
+    return jsonify(employee)
+
+
+@app.route("/employees/<int:id>", methods=["DELETE"])
+def delete_employee(id: int):
+    global employees
+    employee = get_employee(id)
+    if employee is None:
+        return jsonify({"error": "Employee does not exist."}), 404
+    employee = [e for e in employees if e["id"] != id]
+    return jsonify(employee), 200
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5000)
